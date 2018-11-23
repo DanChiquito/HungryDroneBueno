6b8df28a72836d0e5d3669ae43d3bba3b720258d
ITAN ENRIQUE ROMO LOPEZ ARCE <itanrmlpzrc@comunidad.unam.mx>
2018-11-23 14:54:30 -0600

    Pagina control

--- HungryDrone/formCheckDrone.Designer.cs
+++ HungryDrone/formCheckDrone.Designer.cs
@@ -38,13 +38,38 @@
             this.label4 = new System.Windows.Forms.Label();
             this.lbAlt = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.conectarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.puertosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.cbPuertos = new System.Windows.Forms.ToolStripComboBox();
             this.actualizarPedidosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbVertical = new System.Windows.Forms.PictureBox();
+            this.pbLateral = new System.Windows.Forms.PictureBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.verificarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.conectarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbDistancia = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbVertical)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLateral)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label2
@@ -132,6 +157,35 @@
             this.menuStrip1.Text = "menuStrip1";
             this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
             // 
+            // conectarToolStripMenuItem
+            // 
+            this.conectarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.puertosToolStripMenuItem,
+            this.verificarToolStripMenuItem,
+            this.conectarToolStripMenuItem1});
+            this.conectarToolStripMenuItem.Name = "conectarToolStripMenuItem";
+            this.conectarToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
+            this.conectarToolStripMenuItem.Text = "Conectar";
+            // 
+            // puertosToolStripMenuItem
+            // 
+            this.puertosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cbPuertos});
+            this.puertosToolStripMenuItem.Name = "puertosToolStripMenuItem";
+            this.puertosToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.puertosToolStripMenuItem.Text = "Puertos";
+            // 
+            // cbPuertos
+            // 
+            this.cbPuertos.Name = "cbPuertos";
+            this.cbPuertos.Size = new System.Drawing.Size(121, 28);
+            // 
+            // actualizarPedidosToolStripMenuItem
+            // 
+            this.actualizarPedidosToolStripMenuItem.Name = "actualizarPedidosToolStripMenuItem";
+            this.actualizarPedidosToolStripMenuItem.Size = new System.Drawing.Size(143, 24);
+            this.actualizarPedidosToolStripMenuItem.Text = "Actualizar Pedidos";
+            // 
             // gMapControl1
             // 
             this.gMapControl1.Bearing = 0F;
@@ -140,7 +194,7 @@
             this.gMapControl1.GrayScaleMode = false;
             this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMapControl1.LevelsKeepInMemmory = 5;
-            this.gMapControl1.Location = new System.Drawing.Point(564, 59);
+            this.gMapControl1.Location = new System.Drawing.Point(551, 54);
             this.gMapControl1.MarkersEnabled = true;
             this.gMapControl1.MaxZoom = 2;
             this.gMapControl1.MinZoom = 2;
@@ -166,41 +220,179 @@
             this.panel1.Size = new System.Drawing.Size(430, 113);
             this.panel1.TabIndex = 8;
             // 
-            // conectarToolStripMenuItem
+            // pb1
             // 
-            this.conectarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.puertosToolStripMenuItem});
-            this.conectarToolStripMenuItem.Name = "conectarToolStripMenuItem";
-            this.conectarToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
-            this.conectarToolStripMenuItem.Text = "Conectar";
+            this.pb1.Image = ((System.Drawing.Image)(resources.GetObject("pb1.Image")));
+            this.pb1.Location = new System.Drawing.Point(47, 453);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(158, 158);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb1.TabIndex = 9;
+            this.pb1.TabStop = false;
             // 
-            // puertosToolStripMenuItem
+            // pb2
             // 
-            this.puertosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.cbPuertos});
-            this.puertosToolStripMenuItem.Name = "puertosToolStripMenuItem";
-            this.puertosToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
-            this.puertosToolStripMenuItem.Text = "Puertos";
+            this.pb2.Image = ((System.Drawing.Image)(resources.GetObject("pb2.Image")));
+            this.pb2.Location = new System.Drawing.Point(47, 453);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(158, 158);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb2.TabIndex = 9;
+            this.pb2.TabStop = false;
+            this.pb2.Visible = false;
             // 
-            // cbPuertos
+            // pb3
             // 
-            this.cbPuertos.Name = "cbPuertos";
-            this.cbPuertos.Size = new System.Drawing.Size(121, 28);
+            this.pb3.Image = ((System.Drawing.Image)(resources.GetObject("pb3.Image")));
+            this.pb3.Location = new System.Drawing.Point(47, 453);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(158, 158);
+            this.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb3.TabIndex = 9;
+            this.pb3.TabStop = false;
+            this.pb3.Visible = false;
             // 
-            // actualizarPedidosToolStripMenuItem
+            // pb4
             // 
-            this.actualizarPedidosToolStripMenuItem.Name = "actualizarPedidosToolStripMenuItem";
-            this.actualizarPedidosToolStripMenuItem.Size = new System.Drawing.Size(143, 24);
-            this.actualizarPedidosToolStripMenuItem.Text = "Actualizar Pedidos";
+            this.pb4.Image = ((System.Drawing.Image)(resources.GetObject("pb4.Image")));
+            this.pb4.Location = new System.Drawing.Point(47, 453);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(158, 158);
+            this.pb4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb4.TabIndex = 9;
+            this.pb4.TabStop = false;
+            this.pb4.Visible = false;
+            // 
+            // pb5
+            // 
+            this.pb5.Image = ((System.Drawing.Image)(resources.GetObject("pb5.Image")));
+            this.pb5.Location = new System.Drawing.Point(47, 453);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(158, 158);
+            this.pb5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb5.TabIndex = 9;
+            this.pb5.TabStop = false;
+            this.pb5.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(47, 592);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(158, 158);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pbVertical
+            // 
+            this.pbVertical.Image = ((System.Drawing.Image)(resources.GetObject("pbVertical.Image")));
+            this.pbVertical.Location = new System.Drawing.Point(348, 323);
+            this.pbVertical.Name = "pbVertical";
+            this.pbVertical.Size = new System.Drawing.Size(158, 158);
+            this.pbVertical.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbVertical.TabIndex = 9;
+            this.pbVertical.TabStop = false;
+            // 
+            // pbLateral
+            // 
+            this.pbLateral.Image = ((System.Drawing.Image)(resources.GetObject("pbLateral.Image")));
+            this.pbLateral.Location = new System.Drawing.Point(657, 592);
+            this.pbLateral.Name = "pbLateral";
+            this.pbLateral.Size = new System.Drawing.Size(158, 158);
+            this.pbLateral.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbLateral.TabIndex = 9;
+            this.pbLateral.TabStop = false;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(1075, 566);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(224, 73);
+            this.btnIniciar.TabIndex = 10;
+            this.btnIniciar.Text = "Iniciar Entrega";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1075, 677);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 73);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Finalizar Entrega";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(348, 592);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(158, 158);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // verificarToolStripMenuItem
+            // 
+            this.verificarToolStripMenuItem.Name = "verificarToolStripMenuItem";
+            this.verificarToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.verificarToolStripMenuItem.Text = "Verificar";
+            // 
+            // conectarToolStripMenuItem1
+            // 
+            this.conectarToolStripMenuItem1.Name = "conectarToolStripMenuItem1";
+            this.conectarToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.conectarToolStripMenuItem1.Text = "Conectar";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 433);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Objeto detectado a:";
+            // 
+            // lbDistancia
+            // 
+            this.lbDistancia.AutoSize = true;
+            this.lbDistancia.Location = new System.Drawing.Point(160, 433);
+            this.lbDistancia.Name = "lbDistancia";
+            this.lbDistancia.Size = new System.Drawing.Size(16, 17);
+            this.lbDistancia.TabIndex = 1;
+            this.lbDistancia.Text = "d";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(189, 433);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 17);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "[cm]";
             // 
             // formCheckDrone
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(1459, 800);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.pbLateral);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pbVertical);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbDistancia);
             this.Controls.Add(this.lbAlt);
             this.Controls.Add(this.lbVel);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lbLong);
@@ -215,6 +407,15 @@
             this.Load += new System.EventHandler(this.formCheckDrone_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbVertical)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLateral)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -237,5 +438,21 @@
         private System.Windows.Forms.ToolStripComboBox cbPuertos;
         private System.Windows.Forms.ToolStripMenuItem actualizarPedidosToolStripMenuItem;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbVertical;
+        private System.Windows.Forms.PictureBox pbLateral;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolStripMenuItem verificarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem conectarToolStripMenuItem1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbDistancia;
+        private System.Windows.Forms.Label label6;
     }
 }
