6c74f1d04709c7c554395b9e86488000e98951fe
natalymazariegos <nataly_mazariegosp@hotmail.com>
2018-11-25 09:46:25 -0600

    Abierto y cerrado diseño

--- HungryDrone/formInicio.Designer.cs
+++ HungryDrone/formInicio.Designer.cs
@@ -46,8 +46,8 @@
             this.gMapControl1.GrayScaleMode = false;
             this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMapControl1.LevelsKeepInMemmory = 5;
-            this.gMapControl1.Location = new System.Drawing.Point(110, 169);
-            this.gMapControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gMapControl1.Location = new System.Drawing.Point(82, 137);
+            this.gMapControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.gMapControl1.MarkersEnabled = true;
             this.gMapControl1.MaxZoom = 2;
             this.gMapControl1.MinZoom = 2;
@@ -61,7 +61,7 @@
             this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gMapControl1.ShowTileGridLines = false;
-            this.gMapControl1.Size = new System.Drawing.Size(377, 273);
+            this.gMapControl1.Size = new System.Drawing.Size(283, 222);
             this.gMapControl1.TabIndex = 8;
             this.gMapControl1.Zoom = 0D;
             this.gMapControl1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gMapControl1_MouseClick);
@@ -71,10 +71,9 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(201, 126);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(151, 102);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(191, 38);
+            this.label2.Size = new System.Drawing.Size(155, 31);
             this.label2.TabIndex = 5;
             this.label2.Text = "Ingrese su ubicación";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -84,10 +83,9 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(201, 46);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(151, 37);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(191, 41);
+            this.label3.Size = new System.Drawing.Size(143, 33);
             this.label3.TabIndex = 6;
             this.label3.Text = "Ingrese su nombre";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -95,20 +93,24 @@
             // txtbNombre
             // 
             this.txtbNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtbNombre.Location = new System.Drawing.Point(88, 92);
-            this.txtbNombre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtbNombre.Location = new System.Drawing.Point(82, 75);
+            this.txtbNombre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtbNombre.Name = "txtbNombre";
-            this.txtbNombre.Size = new System.Drawing.Size(415, 29);
+            this.txtbNombre.Size = new System.Drawing.Size(283, 24);
             this.txtbNombre.TabIndex = 7;
             // 
             // btnGuardarDatos
             // 
-            this.btnGuardarDatos.Location = new System.Drawing.Point(206, 450);
+            this.btnGuardarDatos.BackColor = System.Drawing.Color.DarkRed;
+            this.btnGuardarDatos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardarDatos.ForeColor = System.Drawing.Color.White;
+            this.btnGuardarDatos.Location = new System.Drawing.Point(82, 366);
+            this.btnGuardarDatos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnGuardarDatos.Name = "btnGuardarDatos";
-            this.btnGuardarDatos.Size = new System.Drawing.Size(181, 31);
+            this.btnGuardarDatos.Size = new System.Drawing.Size(283, 25);
             this.btnGuardarDatos.TabIndex = 9;
             this.btnGuardarDatos.Text = "Guardar Datos";
-            this.btnGuardarDatos.UseVisualStyleBackColor = true;
+            this.btnGuardarDatos.UseVisualStyleBackColor = false;
             this.btnGuardarDatos.Click += new System.EventHandler(this.btnGuardarDatos_Click);
             // 
             // errorProvider1
@@ -117,17 +119,16 @@
             // 
             // formInicio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(599, 518);
+            this.ClientSize = new System.Drawing.Size(449, 421);
             this.Controls.Add(this.btnGuardarDatos);
             this.Controls.Add(this.gMapControl1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtbNombre);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "formInicio";
             this.Text = "formInicio";
             this.Load += new System.EventHandler(this.formInicio_Load);
